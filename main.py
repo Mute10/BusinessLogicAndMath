556c85825132c8235003016e73c4917c49d6ace4
Mute10 <steelgeared@gmail.com>
2025-08-14 21:53:42 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import math
-import string
 from fastapi import FastAPI
-import functools
-import fractions
 import random
+from fractions import Fraction
+from decimal import Decimal
 
 app = FastAPI()
 
@@ -11,29 +10,21 @@ def main():
      return 
 
 class AccountInfo:
- try:
-    def __init__(self, low: 1, high: 50):
-        self.low = low
-        self.high = high
-        def solution_A(acc=list[dict], num=int) -> float:
+    try:
+     @staticmethod
+     def solution(acc:dict, num:dict) -> dict:
             result = 0
-            risks = ("positive", "negative")
-            finalSolution = float()
+            risks = {"positive", "negative"}
 
-            for n in range(len(acc)):
-                if n >= num:
+            for n in range(0, len(acc), 1):
+                if isinstance(n, dict):
                     result += 1
-                    risks.update("n has reached num")
-                raise ValueError("n can't reach num")
-                    
-            if self.low > acc[n] or self.high > acc[n]:
-                result = n + finalSolution
-                return result
-            solution_A([{"a": 1000, "b": 2000, "c": 3000}], 7) 
- except Exception: print("there's no solution")
+                    risks.add("n is greater or equal to num")
+            return acc
+    except Exception: print("there's no solution")
 
 
-def estimate(highNum:int, lowNum:int) -> int:
+    def estimate(highNum:int, lowNum:int) -> int:
      result = 0
      report = str("")
      num = int()
@@ -46,10 +37,7 @@ def estimate(highNum:int, lowNum:int) -> int:
           result = math.floor((result + 2) * 4)
           report += str(num > highNum)
      elif lowNum >= num:
-          result = math.ceil
-          transfer  = num
-          value = transfer
-          num = value
+          math.ceil(result)
           lowNum = [int(d) for d in str(num)]
      else:
           result -= 1
@@ -60,33 +48,37 @@ def estimate(highNum:int, lowNum:int) -> int:
 
 
 class Agriculture(AccountInfo):
-    def __init__(self):
-     self.no_quick_fix = "no_quick_fix"
-    def incomeSpike(self, bonus: float) -> float:
-          if not isinstance(bonus (int, float)):
-               raise FloatingPointError("Bonus must be an int")
-          result = 1.0
+    def __init__(self, quickFix):
+        self.quickFix = quickFix
+    def incomeSpike(self, bonus: float):
+          if not isinstance(bonus, (float, int)):
+               raise TypeError("Bonus must be a float")
+          return bonus
+    
+    def geometry(self):
+          result = {}
+          endResult = 2
           statusReport = {"Janurary-March": 5000,
                           "April-June": 10000,
                           "July-September": 17000,
                           "October-December": 2000}
-          for i in range(len(statusReport)):
-               updatedReport = dict(filter(lambda item: item[0] > 1, statusReport.items()))
-               while updatedReport:
-                    key, scrappedStat = updatedReport.popitem()
-    
-                    if bonus > 5000:
-                         result += 1.2
-                         updatedResult = result * 3.2
-                    else:
-                         updatedResult % 2 == 0
-                         del bonus
-               return updatedResult
+          updateValue = 0
+          updatedReport = dict(statusReport).copy()
+          for key, value in statusReport:
+                    if value > 5000:
+                         statusReport.popitem()
+                         result.update(statusReport)
+                    elif len(key) > 5:
+                         short_key = key[:5]
 
-class Distance(Agriculture):#Name error: Agriculture isn't deifned. Solved: always use a constructor 
-     def __init__(self):
-        super().__init__()
-        self.toTravel = "sqMiles"
+          return (updateValue, updatedReport)
+
+
+
+class Distance(Agriculture):
+     def __init__(self, quickFix):
+        self.quickFix = quickFix
+        super().__init__(quickFix)
      def cebuCity(bantayan: float, daanbantayan: float) -> int:
           result = 1
           difference = abs(bantayan, daanbantayan)
@@ -94,75 +86,84 @@ class Distance(Agriculture):#Name error: Agriculture isn't deifned. Solved: alwa
                 distance_data = math.floor(difference + result)
                 return distance_data
           raise ZeroDivisionError("Can't divide by 0")
-     print(cebuCity(40.17, 35.63))
-disClass = Distance()
+disClass = Distance("==")
 
           
 class FlexMath:
-     def __init__(self, mil, bil):
-          self.mil = 1000000
-          self.bil = 1000000000
+     @staticmethod
      def advancedMath(solved: list[float], unsolved: list[float]) -> int:
-          result = []
-          alpha = [0.9, -0.10, 9, 12]
-          beta = [[4.4, 1, 1, 0], [2.2, 1.1, 0.2, 0.3]]
-          gamma = {90.90, 9.9, 2.77, 300.00}
-          for i in range(0, solved, 1):
-               for j in range(0, unsolved, -1):
-                    if i >= 20:
-                         omega = list(filter(lambda float: float %2 == 0, alpha))
-                         result.append(omega)
-                    elif i < 20:
-                         mega = list(filter(lambda number: number %2 == 1, beta))
-                         result.append(mega)
-                    elif gamma > alpha:
-                         result.extend(4000, 4000)
-                    else:
-                         return 0
-                    if j > 20:
-                         result.append(unsolved[j])
-                    elif j > 40:
-                         result.append(unsolved[i][j])
-                    else:
-                         return None
-               return result
-          raise TypeError("result Isn't an integer")
-
-passingGrade = 80
-math_problems = 50
-points = 2
-total_score = math_problems * points
-requirements = passingGrade // points
-studentGroup = {"Mike": 80,
-                "Brad": 31,
-                "Mae": 70,
-                "jackBlack": 44,
-                "Patrick": 58,
-                "Chris": 17,
-                "Ayano": 84}
-if studentGroup > 88:
-     raise Exception("A+")
-elif studentGroup >= 78:
-     raise Exception("A")
-elif studentGroup >= 70:
-     raise Exception("A-")
-elif studentGroup >= 50:
-     raise Exception("B")
-elif studentGroup >= 40:
-     raise Exception("B-")
-else:
-     raise Exception("you're a dunce")
-               
-          
-advancedSolution = FlexMath()           
-
+               result = []
+               alpha = [0.9, -0.10, 9, 12]
+               beta = [[4.4, 1, 1, 0], [2.2, 1.1, 0.2, 0.3]]
+               gamma = {90.90, 9.9, 2.77, 300.00}
+               star = {2, 2, 2, 2}
+               for i in range(len(solved)):
+                    for j in range(len(unsolved) -1, -1, -1):
+                         if i >= 20:
+                              omega = list(filter(lambda float: float %2 == 0, alpha))
+                              result.append(omega)
+                         elif i < 20:
+                              mega = []
+                              for sublist in beta:
+                                mega.extend(filter(lambda number: number %2 == 1, sublist))
+                              result.append(mega)
+                         elif sum(gamma) > sum(star):
+                              result.extend([4000, 4000])
+                         else:
+                              return 0
                          
-                        
+                         if j > 40:
+                            if isinstance(unsolved[i], list):
+                                   result.append(unsolved[i][j])
+                            else: 
+                                 result.append(0)
+                         elif j > 20:
+                              result.append(unsolved[j])
+                         else: 
+                              return None
+               raise TypeError("result Isn't an integer")
+     
+Sol = FlexMath() 
+adSol = Sol.advancedMath([1.1] * 8**2, [1.2] * 8**2)
 
-   
+
+
+class MathBiome:
+     @staticmethod
+     def magicalChaos(goodNum, badNum):
+          f1 = Fraction(22/300)
+          f2 = Fraction(17*4) / Fraction(1717/7)
+          f3 = Fraction(120/8) + Fraction(19/2)
+          dec = Decimal(0.2/3.13) - Decimal(-1 * -19) #-18.93610223642172522839910442
+          if f1 >= f2:
+               dec = []
+               dec.append(random.randint(1, 100))
+          elif f1 >= f3:
+               dec = []
+               dec.append(random.random())
+          else:
+              tvl = f1 * f2
+          return f3
+MathBiome.magicalChaos(2, 4)
+          
+
+"""""
+math.sqrt(x)      # square root
+math.pow(x, y)    # x to the power of y
+math.floor(x)     # round down
+math.ceil(x)      # round up
+math.sin(x)       # sine (x in radians)
+math.cos(x)       # cosine
+math.log(x)       # natural log
+
+abs(x)       # absolute value
+round(x, n)  # round to n decimal places
+min(a, b, c)
+max(a, b, c)
+"""
+
 if __name__ == "main" :
-            main()
-            acc_info = AccountInfo
-            print(acc_info.solution_A([{"a": 1000, "b": 2000, "c": 3000}], 7))
-            print(acc_info.estimate(1, 2))
-            print(acc_info.incomeSpike())
+
+            
+     main()
+            
